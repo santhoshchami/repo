ae69305e1af5d731000b74841eb68c5b787b4768
santhoshchami <35686375+santhoshchami@users.noreply.github.com>
2018-01-24 15:21:40 +0530

    Update default.rb

--- default.rb
+++ default.rb
@@ -4,7 +4,7 @@
 #
 # Copyright:: 2018, The Authors, All Rights Reserved.
 file '/var/www/html/index.html' do
-  content '<html>This is a git generated file and modified.</html>'
+  content '<html>This is a git generated file and modified2.</html>'
   mode '0755'
   owner 'apache'
   group 'apache'
